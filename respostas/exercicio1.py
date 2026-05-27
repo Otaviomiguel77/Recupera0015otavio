3e5a11c41a1731e20010e9a0353b98a8cfdbff1b
Otavio <tataumiguel860@gmail.com>
2026-05-26 21:30:55 -0300

    Criaçao exercicio1.py

---
+++ respostas/exercicio1.py
@@ -0,0 +1,16 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+print(f"Olá {nome}, você possui {idade} anos.")
+
+
+#nota no aluno
+def calculo_media(nota1, nota2):
+     return (nota1 + nota2) /2
+
+
+media_calculada = calcular_media(nota1=n1, nota2=n2)
+
+print("O aluno {aluno} obteve a seguinte media: {media_calculada")
+
+
